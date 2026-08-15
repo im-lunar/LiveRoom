9fa1b4ca8d4531102d788f2825c3519c63768742
im-lunar <itslunarwrld@gmail.com>
2026-08-15 18:30:44 +0530

    protocol error fixed

--- src/index.ts
+++ src/index.ts
@@ -46,9 +46,10 @@ wsServer.on('request', function(request: any) {
       return;
     }
     
-    var connection = request.accept('echo-protocol', request.origin);
+    var connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message: any) {
+        console.log(message);
         try {
             messageHandler(connection, JSON.parse(message.utf8Data));
         } catch(e) {
